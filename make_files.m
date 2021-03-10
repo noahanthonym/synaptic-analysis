a94603f8b5a4adb6308189dd524b96c007231ada
noahanthonym <78045059+noahanthonym@users.noreply.github.com>
2021-03-09 19:29:57 -0500

    Update make_files.m
    Updated top instructions

--- make_files.m
+++ make_files.m
@@ -1,7 +1,6 @@
-% use this script to make files for analysis. Before running this script,
-% create an empty folder where the data will be saved. 
-
-%unfinished and developmental only
+% Use this script to make files for analysis. Before running this script,
+%   create an empty folder where the data will be saved. 
+% Reminder: For heatmap, one file is needed with your data numbers and data text'
 
 clear
 close all
@@ -15,7 +14,7 @@ import_arrays = str2num(cell2mat(inputdlg('Enter the number of arrays to import'
 cd(import_path)
 for stepper = 1:import_arrays;
     file_nm = inputdlg('Enter the file name you want to use for the data');
-    file_nm = char(file_nm); %% turns dlg_nm into a text scalar
+    file_nm = char(file_nm);
     imported_data = uiimport(import_file); 
     cd(save_folder)
     save(file_nm , 'imported_data');
